16f094ac01d3546ea68aea7ba16fe15494f6031d
Travis Evans <evansgtravis@gmail.com>
2024-02-29 16:34:02 -0500

    Winning combo array added.

--- game.js
+++ game.js
@@ -1,4 +1,16 @@
 let currentPlayer = 1;
+const winningCombos = [
+    [true,true, true, false,false,false, false,false,false],
+    [false,false, false, true,true,true, false,false,false],
+    [false,false, false, false,false,false, true,true,true],
+    [true,false, false, true,false,false, true,false,false],
+    [false,true, false, false,true,false, false,true,false],
+    [false,false, true, false,false,true, false,false,true],
+    [true,false, false, false,true,false, false,false,true],
+    [false,false, true, false,true,false, true,false,false]
+]
+let playerOne = [false,false,false, false,false,false, false,false,false];
+let playerTwo = [false,false,false, false,false,false, false,false,false];
 
 restart();
 
