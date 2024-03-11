ebcd0bf9137ec03ecef4928658b420acf709fd88
Travis Evans <evansgtravis@gmail.com>
2024-03-11 16:30:23 -0400

    Added onto

--- game.js
+++ game.js
@@ -1,43 +1,6 @@
-let currentPlayer = 1;
-const winningCombos = [
-    [true,true, true, false,false,false, false,false,false],
-    [false,false, false, true,true,true, false,false,false],
-    [false,false, false, false,false,false, true,true,true],
-    [true,false, false, true,false,false, true,false,false],
-    [false,true, false, false,true,false, false,true,false],
-    [false,false, true, false,false,true, false,false,true],
-    [true,false, false, false,true,false, false,false,true],
-    [false,false, true, false,true,false, true,false,false]
-]
-let playerOne = [false,false,false, false,false,false, false,false,false];
-let playerTwo = [false,false,false, false,false,false, false,false,false];
-
-restart();
-
-$('.game-board > div').click(function(element) {
-    if (!element.target.innerHTML) {
-        if (currentPlayer == 1) {
-            element.target.innerHTML = 'x';
-            currentPlayer = 2;
-            $('.player-turn').text('Player 2 Turn (O)');
-        }
-        else {
-            element.target.innerHTML = 'o';
-            currentPlayer = 1;
-            $('.player-turn').text('Player 1 Turn (X)');
-        }
-    }
-
-    checkWin();
-})
-
-$('#restart-btn').click(function(){
-    restart();
-})
-
-function checkWin(player) {
+let currentPlayer = 'x';
+let gameOver = false;
 // In Tic Tac Toe, there are eight possible ways to win:
-
 // Three in a row horizontally on the top row.
 // Three in a row horizontally on the middle row.
 // Three in a row horizontally on the bottom row.
@@ -46,13 +9,65 @@ function checkWin(player) {
 // Three in a column on the right column.
 // Three in a diagonal from the top left to the bottom right.
 // Three in a diagonal from the top right to the bottom left.
+const winningCombos = [
+    // [true,true, true, false,false,false, false,false,false],
+    [0,1,2],
+    // [false,false, false, true,true,true, false,false,false],
+    [3,4,5],
+    // [false,false, false, false,false,false, true,true,true],
+    [6,7,8],
+    // [true,false, false, true,false,false, true,false,false],
+    [0,3,6],
+    // [false,true, false, false,true,false, false,true,false],
+    [1,4,7],
+    // [false,false, true, false,false,true, false,false,true],
+    [2,5,8],
+    // [true,false, false, false,true,false, false,false,true],
+    [0,4,8],
+    // [false,false, true, false,true,false, true,false,false]
+    [2,4,6]
+]
+let playerX = [false,false,false, false,false,false, false,false,false];
+let playerO = [false,false,false, false,false,false, false,false,false];
 
-// Take the tiles for the current player (player)
+restart();
 
+$('.game-board > div').click(function(element) {
+    if (!element.target.innerHTML && !gameOver) {
+        // Set tile to player tile
+        element.target.innerHTML = currentPlayer;
+
+        checkWin(currentPlayer, function(won, winningCombo) {
+            // Check for win
+            if (won) {
+                $('.player-turn').text('Player ' + currentPlayer.toUpperCase() + ' Wins!');
+                gameOver = true
+            } else {
+                // Change turn to next player
+                if (currentPlayer == 'x') {
+                    currentPlayer = 'o';
+                    $('.player-turn').text('Player O Turn');
+                } else {
+                    currentPlayer = 'x';
+                    $('.player-turn').text('Player X Turn');
+                }
+            }
+        })
+    }
+})
+
+$('#restart-btn').click(function(){
+    restart();
+})
+
+function checkWin(player, callback) {
+    // player will be 'x' or 'o'
+    
 }
 
 function restart() {
     currentPlayer = 1;
-    $('.player-turn').text('Player 1 to Start (X)');
+    gameOver = false;
+    $('.player-turn').text('Player X to Start');
     $('.game-board > div').text('');
 }
